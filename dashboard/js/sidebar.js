9b1b35a656ee6506aa21057accb677d2cb4768b0
Bastián Ñiripil <ba.niripil@duocuc.cl>
2024-05-06 16:01:55 -0400

    add forms & validations

--- dashboard/js/sidebar.js
+++ dashboard/js/sidebar.js
@@ -1,34 +1,23 @@
-function openNav() {
-  document.getElementById("content").style.transition = 'margin-left 0.5s';
-  document.getElementById("mySidebar").style.width = "210px";
-  document.getElementById("content").style.marginLeft = "210px";
-}
+$(document).ready(function() {
+  $('#sidebarCollapse').click(function() {
+    console.log('Click en el botón de colapsar la barra lateral');
+    $('#mySidebar').toggleClass('show'); // Alternar la clase 'show' en la barra lateral
+    if ($('#mySidebar').hasClass('show')) {
+      openNav();
+    } else {
+      closeNav();
+    }
+  });
 
-function closeNav() {
-  document.getElementById("content").style.transition = 'margin-left 0.5s';
-  document.getElementById("mySidebar").style.width = "0";
-  document.getElementById("content").style.marginLeft= "0";
-}
+  function openNav() {
+    $('#content').css('transition', 'margin-left 0.5s');
+    $('#mySidebar').css('width', '210px');
+    $('#content').css('margin-left', '210px');
+  }
 
-
-document.addEventListener('DOMContentLoaded', function() {
-  const sidebar = document.getElementById('mySidebar');
-  const content = document.getElementById('content');
-
-  if (sidebar && content) {
-      console.log('Elementos encontrados:', sidebar, content);
-    
-      document.getElementById('sidebarCollapse').addEventListener('click', function() {
-          console.log('Click en el botón de colapsar la barra lateral');
-          sidebar.classList.toggle('show'); // Alternar la clase 'show' en la barra lateral
-          if (sidebar.classList.contains('show')) {
-            openNav();
-            
-          } else {
-            closeNav();
-          }
-      });
-  } else {
-      console.error('No se encontraron elementos necesarios.');
+  function closeNav() {
+    $('#content').css('transition', 'margin-left 0.5s');
+    $('#mySidebar').css('width', '0');
+    $('#content').css('margin-left', '0');
   }
 });
