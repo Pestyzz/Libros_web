7d0b60e893c8ca70426688a336458d52333e0db6
Bastián Maximiliano Ñiripil Concha <ba.niripil@duocuc.cl>
2024-05-27 09:00:40 -0400

    initial commit

--- dashboard/js/navlink.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function () {
-    let url = window.location;
-    $('ul.navbar-nav a[href="' + url + '"]').addClass('active');
-    $('ul.navbar-nav a').filter(function() {
-        return this.href == url;
-    }).addClass('active');
- });
