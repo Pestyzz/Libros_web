8ab23af580046f06d2b4836d35f8f4efdf100bd2
Bastián Maximiliano Ñiripil Concha <ba.niripil@duocuc.cl>
2024-06-03 02:10:57 -0400

    implement: recover form + adjust

--- login_app/views.py
+++ login_app/views.py
@@ -6,7 +6,10 @@ def login(request):
     return render(request, 'login.html')
 
 def register(request):
-    return render(request, 'register.html')
+    context = {
+        'register': True
+    }
+    return render(request, 'register.html', context)
 
 def recover(request):
     return render(request, 'recover.html')
