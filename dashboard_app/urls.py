000095b2fab0a6187d23432f00413c99fab797c0
Bastián Maximiliano Ñiripil Concha <ba.niripil@duocuc.cl>
2024-06-03 10:04:39 -0400

    implement: dashboard products page + product add form + product edit form

--- dashboard_app/urls.py
+++ dashboard_app/urls.py
@@ -2,5 +2,14 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.dashboard, name='dashboardHome')
+    path('', views.dashboard, name='dashboardHome'),
+    path('charts/', views.dashboardCharts, name='dashboardCharts'),
+    path('shopping/', views.dashboardShopping, name='dashboardShopping'),
+    path('shopping_edit/', views.dashboardShoppingEdit, name='dashboardShoppingEdit'),
+    path('orders/', views.dashboardOrders, name='dashboardOrders'),
+    path('products/', views.dashboardProducts, name='dashboardProducts'),
+    path('product_add/', views.dashboardProductAdd, name='dashboardProductAdd'),
+    path('product_edit/', views.dashboardProductEdit, name='dashboardProductEdit'),
+    path('users/', views.dashboardUsers, name='dashboardUsers'),
+    path('user_add/', views.dashboardUserAdd, name='dashboardUserAdd')
 ]
--- dashboard_app/views.py
+++ dashboard_app/views.py
@@ -4,3 +4,30 @@ from django.shortcuts import render
 
 def dashboard(request):
     return render(request, 'dashboard.html')
+
+def dashboardCharts(request):
+    return render(request, 'charts.html')
+
+def dashboardShopping(request):
+    return render(request, 'shopping/shopping.html')
+
+def dashboardShoppingEdit(request):
+    return render(request, 'shopping/shoppingedit.html')
+
+def dashboardOrders(request):
+    return render(request, 'orders.html')
+
+def dashboardProducts(request):
+    return render(request, 'products/products.html')
+
+def dashboardProductAdd(request):
+    return render(request, 'products/productadd.html')
+
+def dashboardProductEdit(request):
+    return render(request, 'products/productedit.html')
+
+def dashboardUsers(request):
+    return render(request, 'users/users.html')
+
+def dashboardUserAdd(request):
+    return render(request, 'users/useradd.html')
