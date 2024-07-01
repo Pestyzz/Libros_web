9e64ea4dfe8c657900f5c756bc96a909dee5ab80
CETECOM <cetecom_conce@duoc.cl>
2024-07-01 08:54:41 -0400

    finished

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\basti\Dropbox\Duoc Uc\1° Semestre\Programación Web\Libros_web\venv
+set VIRTUAL_ENV=C:\Users\CETECOM\Libros_web\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
